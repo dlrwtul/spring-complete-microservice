8833540e7b9af6d48e105d2617b297aff9e50eea
dlrwtul <dibrahimalang@gmail.com>
2024-09-04 15:02:58 +0000

    done

--- lut-api-gateway/src/main/java/com/wrld/lut_api_gateway/configs/SecurityConfig.java
+++ lut-api-gateway/src/main/java/com/wrld/lut_api_gateway/configs/SecurityConfig.java
@@ -4,20 +4,16 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -32,8 +28,6 @@ import reactor.core.publisher.Mono;
 @EnableReactiveMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-  @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-  String jwkSetUri;
 
   @Bean
   public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
@@ -72,11 +66,5 @@ public class SecurityConfig {
         .map(SimpleGrantedAuthority::new)
         .collect(Collectors.toList());
   }
-
-  // @Bean
-  // public ReactiveJwtDecoder jwtDecoder() {
-  // return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri)
-  // .build();
-  // }
 
 }
