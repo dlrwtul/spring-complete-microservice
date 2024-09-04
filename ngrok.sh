a1c07592ff5b3a1d9ba7f3dbb575a3e47019d2c1
dlrwtul <dibrahimalang@gmail.com>
2024-09-04 15:10:08 +0000

    done

--- ngrok.sh
+++ ngrok.sh
@@ -1,2 +1,5 @@
 docker pull ngrok/ngrok
-docker run --net=host -it -e NGROK_AUTHTOKEN=TOKEN ngrok/ngrok:latest http 8083
+
+# open ngrok account and replace [TOKEN] by your given token 
+
+docker run --net=host -it -e NGROK_AUTHTOKEN=[TOKEN] ngrok/ngrok:latest http 8083
